6290de939cc0af5966e8b0a284fc3c7d6868e908
sebastia <sebastia>
2026-09-04 09:00:13 -0500

    se adisiono el DTO de entrada

---
+++ final/src/main/java/com/example/demo/dto/ProductRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+public class ProductRequestDTO {
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer cantidadDisponible;
+    private String categoria;
+
+    public ProductRequestDTO() {}
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public Double getPrecio() { return precio; }
+    public void setPrecio(Double precio) { this.precio = precio; }
+    public Integer getCantidadDisponible() { return cantidadDisponible; }
+    public void setCantidadDisponible(Integer cantidadDisponible) { this.cantidadDisponible = cantidadDisponible; }
+    public String getCategoria() { return categoria; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+}
