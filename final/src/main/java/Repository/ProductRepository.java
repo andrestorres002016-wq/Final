43b001dcebac89b68a8a69116a226712393a1f47
sebastia <sebastia>
2026-09-04 09:37:16 -0500

    se corrigio un error que salia en repositorio

--- final/src/main/java/Repository/ProductRepository.java
+++ final/src/main/java/Repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package Repository;
 
 import com.example.demo.Model.ProductModel;
-import com.example.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
