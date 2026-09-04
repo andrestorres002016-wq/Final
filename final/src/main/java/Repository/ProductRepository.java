94abd5ca8056386d0abb06a9a70cc86a3e244bd2
sebastia <sebastia>
2026-09-04 09:22:37 -0500

    se adisiono el repositorio

---
+++ final/src/main/java/Repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import com.example.demo.Model.ProductModel;
+import com.example.demo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+    List<ProductModel> findByCategoriaIgnoreCase(String categoria);
+    List<ProductModel> findByNombreContainingIgnoreCase(String nombre);
+}
